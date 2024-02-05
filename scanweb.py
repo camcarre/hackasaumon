bdc127830af8bbc0446c6db866c2f0063ccaf14b
theo.delporte@ynov.com <theo.delporte@ynov.com>
2024-02-05 14:34:47 +0100

    solution avec History.db sans actualisation

--- scanweb.py
+++ scanweb.py
@@ -1,29 +1,17 @@
-import os
-import json
+import sqlite3
 
-indexed_db_path = "C:/Users/delpo/AppData/Local/Google/Chrome/User Data/Default/IndexedDB"
+# Connexion à la base de données
+conn = sqlite3.connect("History.db")
+cursor = conn.cursor()
 
-def parcourir_dossier(dossier):
-    for element in os.listdir(dossier):
-        chemin = os.path.join(dossier, element)
-        if os.path.isdir(chemin):
-            parcourir_dossier(chemin)
-        else:
-            try:
-                with open(chemin, "r", encoding="utf-8") as fichier:
-                    contenu = fichier.read()
-                    try:
-                        data = json.loads(contenu)
-                        if 'entries' in data:
-                            for entry in data['entries']:
-                                if 'url' in entry:
-                                    print(entry['url'])
-                    except json.JSONDecodeError:
-                        pass
-            except UnicodeDecodeError:
-                print(f"Ignoré le fichier {chemin} car il ne peut pas être décodé avec UTF-8")
-                pass
+# Requête pour sélectionner les URL visitées
+cursor.execute("SELECT url, title, visit_count FROM urls")
+urls = cursor.fetchall()
 
+# Affichage des URL visitées
+for url in urls:
+    print(url)
 
-parcourir_dossier(indexed_db_path)
+# Fermeture de la connexion
+conn.close()
 
