255fb2fb43bf6ee376663e7410fc16ad609eaa6d
theo.delporte@ynov.com <theo.delporte@ynov.com>
2024-02-05 13:53:33 +0100

    essai

--- scanweb.py
+++ scanweb.py
@@ -1,14 +1,29 @@
-import sqlite3
+import os
+import json
 
-chrome_history_path = r"C:\Users\YourUsername\AppData\Local\Google\Chrome\User Data\Default\History"
+indexed_db_path = "C:/Users/delpo/AppData/Local/Google/Chrome/User Data/Default/IndexedDB"
 
-conn = sqlite3.connect(chrome_history_path)
-cursor = conn.cursor()
+def parcourir_dossier(dossier):
+    for element in os.listdir(dossier):
+        chemin = os.path.join(dossier, element)
+        if os.path.isdir(chemin):
+            parcourir_dossier(chemin)
+        else:
+            try:
+                with open(chemin, "r", encoding="utf-8") as fichier:
+                    contenu = fichier.read()
+                    try:
+                        data = json.loads(contenu)
+                        if 'entries' in data:
+                            for entry in data['entries']:
+                                if 'url' in entry:
+                                    print(entry['url'])
+                    except json.JSONDecodeError:
+                        pass
+            except UnicodeDecodeError:
+                print(f"Ignoré le fichier {chemin} car il ne peut pas être décodé avec UTF-8")
+                pass
 
-cursor.execute("SELECT url, title, visit_count FROM urls")
-urls = cursor.fetchall()
 
-for url in urls:
-    print(url)
+parcourir_dossier(indexed_db_path)
 
-conn.close()
