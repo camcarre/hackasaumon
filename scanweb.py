c0e887c00ef84a720cd65be02cce4544174eb76a
theo.delporte@ynov.com <theo.delporte@ynov.com>
2024-02-06 11:12:26 +0100

    modif pour obtenir seulement les 20 dernier url

--- scanweb.py
+++ scanweb.py
@@ -1,17 +1,19 @@
 import sqlite3
+import time
+import datetime
 
-# Connexion à la base de données
 conn = sqlite3.connect("History.db")
 cursor = conn.cursor()
 
-# Requête pour sélectionner les URL visitées
-cursor.execute("SELECT url, title, visit_count FROM urls")
+start_of_day = int(datetime.datetime.combine(datetime.date.today(), datetime.time.min).timestamp())
+
+cursor.execute("SELECT urls.url, urls.title, urls.visit_count FROM urls INNER JOIN visits ON urls.id = visits.url WHERE visits.visit_time >= ?", (start_of_day,))
 urls = cursor.fetchall()
 
-# Affichage des URL visitées
-for url in urls:
-    print(url)
-
-# Fermeture de la connexion
+urls_today = [url for url in urls if url[2] >= start_of_day]
+for i in urls[-20:]:
+    print(i)
 conn.close()
+
+
 
