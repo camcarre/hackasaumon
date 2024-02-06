5f5f7a0fcae14497391300536d557da83840b754
theo.delporte@ynov.com <theo.delporte@ynov.com>
2024-02-06 12:05:56 +0100

    supression des doublons des url, et je mange du pain

--- scanweb.py
+++ scanweb.py
@@ -1,6 +1,7 @@
 import sqlite3
 import time
 import datetime
+import copy
 
 conn = sqlite3.connect("History.db")
 cursor = conn.cursor()
@@ -9,11 +10,19 @@ start_of_day = int(datetime.datetime.combine(datetime.date.today(), datetime.tim
 
 cursor.execute("SELECT urls.url, urls.title, urls.visit_count FROM urls INNER JOIN visits ON urls.id = visits.url WHERE visits.visit_time >= ?", (start_of_day,))
 urls = cursor.fetchall()
+urls = urls[-20:]
 
-urls_today = [url for url in urls if url[2] >= start_of_day]
-for i in urls[-20:]:
-    print(i)
-conn.close()
+def doublon(list):
+    url = []
+    final = []
+    for i in list:
+        if i[0] not in url:
+            url.append(i[0])
+            final.append(i)
+    return final
+urls = doublon(urls)
 
-
+for i in urls:
+    print(i[1])
+    print(i[0])
 
